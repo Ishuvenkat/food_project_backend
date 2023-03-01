6f18735e327bf3ba02c55c9fbe4dfeaf66978aea
ishu <ishuvenkat1313@gmail.com>
2023-03-01 16:32:19 +0530

    The entire register api is complete along with naming and crossorigin

--- demo/src/main/java/com/example/demo/controller/controllerclass.java
+++ demo/src/main/java/com/example/demo/controller/controllerclass.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import com.example.demo.response.ResponseClass;
 import com.example.demo.service.*;
 
 @RestController
+@CrossOrigin("localhost:5173")
 @RequestMapping("/home")
 
 public class ControllerClass {
